38e1c83d8533b3977ed60c4922d2a11068be6065
JasrajChouhan <jasrajc945@gmail.com>
2024-12-30 00:53:34 +0530

    (feat): create signin and signup route

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,13 @@
-export default function Home (){
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
   return (
     <div>
       <h1 className="text-3xl underline bg-red-400" >
         Home
       </h1>
-    hello world</div>
+
+      <UserButton />
+      hello world</div>
   )
 }
