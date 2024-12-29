7b335fac4ea81df458c60c7ebcac499f1a470f20
JasrajChouhan <jasrajc945@gmail.com>
2024-12-30 05:24:58 +0530

    (feat): short url creating api

---
+++ app/api/shortner/route.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import { randomUUID } from "crypto";
+import { NextResponse } from "next/server";
+
+// Prisma Client 
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: Request) => {
+  const { longUrl } = await req.json();
+
+  try {
+    if (!longUrl) {
+      return NextResponse.json({ error: 'Invalid URL' }, { status: 400 });
+    }
+
+    const existingUrl = await prisma.url.findFirst({
+      where: {
+        longUrl
+      }
+    })
+
+    if (existingUrl) {
+      return NextResponse.json({ shortUrl: existingUrl.shortUrl });
+    }
+
+    // new url 
+    const shortId = randomUUID({}).substring(0, 6);
+    const shortUrl = `${process.env.BASE_URL}/${shortId}`;
+
+    const newUrl = await prisma.url.create({
+      data: {
+        longUrl,
+        shortUrl,
+        shortId,
+      }
+    })
+
+    return NextResponse.json({ shortUrl: newUrl.shortUrl });
+  } catch (error) {
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
