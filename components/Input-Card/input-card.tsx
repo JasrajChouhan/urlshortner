60200f71f087a49e0270ad65e7fbb91247e11f99
JasrajChouhan <jasrajc945@gmail.com>
2024-12-30 05:46:01 +0530

    (feat): get the short url

--- components/Input-Card/input-card.tsx
+++ components/Input-Card/input-card.tsx
@@ -2,16 +2,42 @@ import { Input } from "@/components/input";
 import { useState } from "react";
 
 export const InputCard = () => {
-  const [value, setValue] = useState<string>("");
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/shortner', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ longUrl }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setShortUrl(data.shortUrl);
+      } else {
+        console.error(data.error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
       <div>
-        <form method="post" className="flex flex-col sm:flex-row items-center gap-2">
+        <form
+          method="post" 
+          className="flex flex-col sm:flex-row items-center gap-2"
+          onSubmit={handleSubmit}
+          >
           <Input
             type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            value={longUrl}
+            onChange={(e) => setLongUrl(e.target.value)}
             variant="outline"
             size="md"
             placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
@@ -20,12 +46,21 @@ export const InputCard = () => {
           <button
             type="submit"
             className="bg-gray-500 text-white px-4 py-2 rounded-md disabled:opacity-50 hover:bg-gray-600 transition"
-            disabled={value === ""}
+            disabled={longUrl === ""}
           >
             Submit
           </button>
         </form>
       </div>
+
+      {shortUrl && (
+        <div>
+          <p>Short URL:</p>
+          <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+            {shortUrl}
+          </a>
+        </div>
+      )}
     </div>
   );
 };
